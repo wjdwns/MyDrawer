ec2b46dfe01968d3bfe02a4036c479fddc2f7d0c
wjdwns <wjdwns3744@naver.com>
2021-04-19 09:57:09 +0900

    4일차

--- app/src/main/java/com/example/mydrawer/MainActivity.kt
+++ app/src/main/java/com/example/mydrawer/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    enum class FragmentItem{
+        ITEM1, ITEM2, ITEM3
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
